2ed5b2241d978de5bc471be2a01b72982d0bd02f
Numerica98 <00041517@uca.edu.sv>
2019-11-04 11:42:25 -0600

    Updates

---
+++ precache-manifest.11ce7fa0a1a54406ab0c6237f9496998.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20bbb6dd311c6ca34ac50ebbab8f7ba",
+    "url": "/registrolab/index.html"
+  },
+  {
+    "revision": "06ba5227464f09860be8",
+    "url": "/registrolab/static/css/main.fda940a2.chunk.css"
+  },
+  {
+    "revision": "64f811bfeeb05c4d6877",
+    "url": "/registrolab/static/js/2.2f6065fe.chunk.js"
+  },
+  {
+    "revision": "06ba5227464f09860be8",
+    "url": "/registrolab/static/js/main.f720f49f.chunk.js"
+  },
+  {
+    "revision": "352bf2ed78e3d2b44824",
+    "url": "/registrolab/static/js/runtime-main.f661c381.js"
+  }
+]);
